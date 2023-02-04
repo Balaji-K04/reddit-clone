dabd19fbcb74fa3ba44e63a1ff8901a2777e114a
Balaji <balajisathesh.bs@gmail.com>
2023-02-04 12:07:35 +0530

    Added function in navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from '../chakra/theme'
-import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
