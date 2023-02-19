40bdfcf5bacd8249f89e8007c2f30f6b113cf469
Balaji Krishnakumar <84065819+Balaji-Sathesh@users.noreply.github.com>
2023-02-19 18:09:15 +0530

    Added packages and added community

--- src/pages/r/[commmunityid]/index.tsx
+++ src/pages/r/[commmunityid]/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { Community } from "@/src/atoms/communitiesAtom";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSideProps } from "next";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
 
-type indexProps = {
-    
+type CommunityPageProps = {
+  communityData: Community;
 };
 
-const index:React.FC<indexProps> = () => {
-    
-    return <div>Have a good coding</div>
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>Welcome to {}</div>;
+};
+
+async function getServerSideProps(context: GetServerSideProps) {
+  //Get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.community as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+        ),
+      },
+    };
+  } catch (error) {
+    //could add error page
+    console.log("getServerSideProps error", error);
+  }
 }
-export default index;
+
+export default CommunityPage;
